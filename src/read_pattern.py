1a14e2eac735f77b6e7862f14530262025bab20e
bjoernwe <bjoernwe@users.noreply.github.com>
2020-03-22 18:51:58 +0100

    ✨ Print pattern as list of integers

--- src/read_pattern.py
+++ src/read_pattern.py
@@ -2,10 +2,25 @@ import numpy as np
 import PIL.Image
 
 
+PATTERN_WIDTH = 27
+PATTERN_N_COLORS = 3
+
+
+def read_patter_from_file(file_name) -> np.array:
+    img = PIL.Image.open(file_name)
+    img = PATTERN_N_COLORS - np.array(img)[:, :PATTERN_WIDTH]
+    return img
+
+
+def print_pattern(img: np.array):
+    for row in img:
+        print(str(list(row)))
+
+
 def main():
     file_name = '../res/pattern.png'
-    img = PIL.Image.open(file_name)
-    print(3-np.array(img))
+    img = read_patter_from_file(file_name)
+    print_pattern(img)
 
 
 if __name__ == '__main__':
